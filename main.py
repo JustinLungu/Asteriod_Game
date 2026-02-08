b156740ccdd8028f7c3695f3ab04aec82e694e52
JustinLungu <iustinl113@gmail.com>
2026-02-08 18:43:16 +0100

    rotate left/right

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from constants import *
 from logger import log_state
+from player import Player
 import pygame
 
 
@@ -13,14 +14,19 @@ def main():
     dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
     while True:
         log_state()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+            
+        player.update(dt)
         
         screen.fill("black")
-        #screen.display.flip()
+        player.draw(screen)
+        pygame.display.flip()
         dt = clock.tick(60) / 1000.0
 
         
